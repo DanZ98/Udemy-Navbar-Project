715bed16f170688fa42f77fb3d1dc755a08c6484
Dan Zins <danzins15@gmail.com>
2021-10-11 21:37:58 -0500

    Completed Navbar JavaScript Project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,18 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// Retrieving classes
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+// Adding click event 
+navToggle.addEventListener('click', function(){
+
+    links.classList.toggle('show-links');
+
+    // Another way of executing toggle effect
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }else {
+    //     links.classList.add('show-links');
+    // }
+
+});
+
